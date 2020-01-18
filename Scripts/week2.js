5d3a9173bc79da283699039e3688bfaaf3e85c01
collinhaydock <59694799+collinhaydock@users.noreply.github.com>
2020-01-18 14:09:08 -0700

    Initial Commit

---
+++ Scripts/week2.js
@@ -0,0 +1,11 @@
+//const name = "Collin Haydock"
+
+//document.getElementById("name").innerHTML = name;
+
+const lastModifiedDate = document.lastModified;
+document.querySelector("#lastModifiedDate").textContent = lastModifiedDate;
+
+function enterName () {
+    const userName = document.getElementById("yourName").value;
+    document.getElementById("name").innerHTML = userName;
+}
